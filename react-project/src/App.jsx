1c6924828a6cb37044ec390b8c34a4d38cb5d27d
Nursultan Dzhumabaev <nurzamazkatanks@gmail.com>
2023-12-01 22:30:43 +0000

    dynamic layout

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,10 +1,16 @@
+const arr = ['A', 'B', 'C'];
+
+function rndInt(max){
+  return Math.floor(Math.random() * (max+1));
+}
+
 function Header(){
   return(
     <header>
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {arr[rndInt(2)]} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
