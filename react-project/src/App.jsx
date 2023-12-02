2c332e2e3cdeb69199d13d568982b0544a02b580
Nursultan Dzhumabaev <nurzamazkatanks@gmail.com>
2023-12-02 22:26:08 +0000

    import image as var

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,3 +1,5 @@
+import img from "./assets/react-core-concepts.png"
+
 const arr = ['A', 'B', 'C'];
 
 function rndInt(max){
@@ -7,7 +9,7 @@ function rndInt(max){
 function Header(){
   return(
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={img} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
         {arr[rndInt(2)]} React concepts you will need for almost any app you are
