2acdb811d6091eb817ba49e15fe3fe628e691ef7
Nursultan Dzhumabaev <nurzamazkatanks@gmail.com>
2023-12-27 23:56:08 +0600

    state

---
+++ patterns/State.cs
@@ -0,0 +1,66 @@
+public interface INotification {
+    void Display();
+}
+
+public class BasicNotification : INotification {
+    private string sender;
+    private DateTime timestamp;
+
+    public BasicNotification(string sender, DateTime timestamp) {
+        this.sender = sender;
+        this.timestamp = timestamp;
+    }
+
+    public void Display() {
+        Console.WriteLine($"Basic Notification from {sender} at {timestamp}");
+    }
+}
+
+
+public class TextNotification : INotification {
+    private BasicNotification basicNotification;
+    private string textContent;
+
+    public TextNotification(BasicNotification basicNotification, string textContent) {
+        this.basicNotification = basicNotification;
+        this.textContent = textContent;
+    }
+
+    public void Display() {
+        basicNotification.Display();
+        Console.WriteLine($"Text Content: {textContent}");
+    }
+}
+
+public class ImageNotification : INotification {
+    private BasicNotification basicNotification;
+    private string imageUrl;
+
+    public ImageNotification(BasicNotification basicNotification, string imageUrl) {
+        this.basicNotification = basicNotification;
+        this.imageUrl = imageUrl;
+    }
+
+    public void Display() {
+        basicNotification.Display();
+        Console.WriteLine($"Image URL: {imageUrl}");
+    }
+}
+
+// Similar state classes for other types of notifications
+
+class Program {
+    static void Main(string[] args) {
+        INotification basicNotification = new BasicNotification("Alice", DateTime.Now);
+
+        // Transition to a TextNotification state
+        INotification textNotification = new TextNotification((BasicNotification)basicNotification, "Hello!");
+        textNotification.Display();
+
+        // Transition to an ImageNotification state
+        INotification imageNotification = new ImageNotification((BasicNotification)basicNotification, "http://example.com/image.jpg");
+        imageNotification.Display();
+
+        // You can easily add more state classes for different notification types
+    }
+}
